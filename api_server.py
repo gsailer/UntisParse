6c166b66609ddcc184d8ebc624a17fc5190293d3
sublinus <sublinus@users.noreply.github.com>
2017-05-02 18:20:22 +0200

    cron mode and requirements

--- api_server.py
+++ api_server.py
@@ -1,25 +1,37 @@
 # flask_api server for vertretung
 from flask import Flask, jsonify, abort
 import json
+import argparse
 import untis_parser
 
-app = Flask(__name__)
+parser = argparse.ArgumentParser(prog="untisparse")
+parser.add_argument('--cron', action='store_true')
+parser.add_argument('-d', '--destination', default="./html")
+args = parser.parse_args()
+
 plan = untis_parser.parseVertretungsplan()
 klassen = set([])
 for x in plan:
 	klassen.add(x)
 
-@app.route("/vertretung", methods=['GET'])
-def vertretungsplan():
-	return json.dumps([ob.__dict__ for ob in plan], ensure_ascii=False)
+if args.cron:
+	with open("{}/vertretung.json".format(args.destination), "w") as f:
+		json.dump([ob.__dict__ for ob in plan], f, ensure_ascii=False)
+else:
+	app = Flask(__name__)
 
-@app.route("/vertretung/<klasse>", methods=['GET'])
-def vertretungsplan_by_klasse(klasse):
-	plan_by_klasse = []
-	for x in plan:
-		if x.klasse == klasse:
-			plan_by_klasse.append(x)
-	return json.dumps([ob.__dict__ for ob in plan_by_klasse], ensure_ascii=False)
 
-if __name__ == "__main__":
-	app.run()
+	@app.route("/vertretung", methods=['GET'])
+	def vertretungsplan():
+		return json.dumps([ob.__dict__ for ob in plan], ensure_ascii=False)
+
+	@app.route("/vertretung/<klasse>", methods=['GET'])
+	def vertretungsplan_by_klasse(klasse):
+		plan_by_klasse = []
+		for x in plan:
+			if x.klasse == klasse:
+				plan_by_klasse.append(x)
+		return json.dumps([ob.__dict__ for ob in plan_by_klasse], ensure_ascii=False)
+
+	if __name__ == "__main__":
+		app.run()
